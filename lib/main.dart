e7c0b15991a0a7acb2aeaaae061b3ee3b5748955
Oreoluwa Bimbo-Salami <oreoluwabs25@gmail.com>
2019-09-13 03:31:25 +0100

    Added Routes

---
+++ lib/main.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+
+import 'package:world_time/pages/home.dart';
+import 'package:world_time/pages/loading.dart';
+import 'package:world_time/pages/choose_location.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      initialRoute: '/home',
+      routes: {
+        '/' : (context) => LoadingPage(),
+        '/home': (context) => HomePage(),
+        '/location' : (context) => ChooseLocationPage(),
+      },
+    );
+  }
+}
---
+++ lib/pages/choose_location.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+
+class ChooseLocationPage extends StatefulWidget {
+  @override
+  _ChooseLocationPageState createState() => _ChooseLocationPageState();
+}
+
+class _ChooseLocationPageState extends State<ChooseLocationPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.grey[200],
+      appBar: AppBar(
+        backgroundColor: Colors.blue[900],
+        title: Text('Choose a Location'),
+        elevation: 0,
+        centerTitle: true,
+      ),
+      body: Text('Choose Location'),
+    );
+  }
+}
